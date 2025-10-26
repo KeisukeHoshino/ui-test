f597bd19acea485202363fc1a829b3d81b7aec6e
KeisukeHoshino <Keisuke.hoshino.pl@gmail.com>
2025-10-26 10:58:42 +0900

    jestの設定修正

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
+/** @type {import("ts-jest").JestConfigWithTsJest} **/
 export default {
   // testの実行環境を指定
   testEnvironment: "jest-environment-jsdom",
   transform: {
-    ...tsJestTransformCfg,
+    "^.+.tsx?$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
   },
   // jest-domの拡張マッチャーを使用可能に
   setupFilesAfterEnv: ["./jest.setup.ts"],
